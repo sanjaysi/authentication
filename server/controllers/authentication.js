bd31534e2f627f164414716eac8b63de31c153eb
sanjaysi <cloudwithpython.com>
2016-09-21 23:46:38 +0530

    Added hash passwd & generated token

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,16 +1,29 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    // jwt provides a sub (subject) property - who is the subject; here it is the user
+    // iat - issued at time
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({email: email}, function(err, existinUser) {
+  User.findOne({email: email}, function(err, existingUser) {
       if (err) {
         return next(err);
       }
 
-      if (existinUser) {
+      if (existingUser) {
         return res.status(422).send({error: 'Email in use'});
+      }
+
+      if (!email || !password) {
+        return res.status(422).send({error: 'Both email and password should be present'});
       }
 
       const user = new User({
@@ -20,7 +33,7 @@ exports.signup = function(req, res, next) {
       user.save(function(err) {
         if (err) { return next(err); }
 
-        res.json({success: true});
+        res.json({token: tokenForUser(user)});
       });
   });
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // define model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// Before saving the model, run this
+userSchema.pre('save', function(next) {
+    const user = this; // set context of function to user i.e. user is instance of user model
+
+    // generate a salt, take some amount of time (millisecond) and hence a callback
+    // is passed to run after salt is generated
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err)  { return next(err); }
+
+      // hash (mean encrypt) the password using the salt; it again take some time, and hence
+      // callback defined on has generated
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) {return next(err); }
+
+        user.password = hash;
+        // next() is to go ahead and save the model
+        next();
+      });
+    });
 });
 
 // create model class
